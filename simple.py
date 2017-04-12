5c1169bb175df5baf4afe011d022fb196a8c43c9
1o0ko <wojciech.stokowiec@gmail.com>
2017-04-12 22:08:35 +0200

    added simple script

---
+++ simple.py
@@ -0,0 +1,19 @@
+'''
+Simple script showcasting Tensorboard
+'''
+import tensorflow as tf
+
+
+def main():
+    ''' Build graph and run it'''
+
+    a = tf.constant(2)
+    b = tf.constant(3)
+    x = tf.add(a, b)
+
+    with tf.Session() as sess:
+        print(sess.run(x))
+
+
+if __name__ == '__main__':
+    main()
