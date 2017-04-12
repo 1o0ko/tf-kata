1942ec53206ed67f37591149d499805a521da846
1o0ko <wojciech.stokowiec@gmail.com>
2017-04-12 22:19:28 +0200

    added graph logging to tensorboard

--- simple.py
+++ simple.py
@@ -12,8 +12,11 @@ def main():
     x = tf.add(a, b)
 
     with tf.Session() as sess:
+        writer = tf.summary.FileWriter('./graphs', sess.graph)
         print(sess.run(x))
 
+    # close the writer when you’re done using it
+    writer.close()
 
 if __name__ == '__main__':
     main()
