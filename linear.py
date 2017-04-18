d32821b8d8a7f1550ee84de1fdda937c6ab21063
1o0ko <wojciech.stokowiec@gmail.com>
2017-04-18 20:34:48 +0200

    finished linear regression

--- linear.py
+++ linear.py
@@ -1,22 +1,79 @@
-'''
-Simple linear reggression in Tensorflow
-'''
-import tensorflow as tf
+"""
+Simple linear regression example in TensorFlow
+This program tries to predict the number of thefts from
+the number of fire in the city of Chicago
+"""
 
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import xlrd
+
+DATA_FILE = 'data/fire_theft.xls'
 
 def main():
-    ''' Build graph and run it'''
+    # Phase 1: Assemble the graph
+    # Step 1: read in data from the .xls file
+    book = xlrd.open_workbook(DATA_FILE, encoding_override='utf-8')
+    sheet = book.sheet_by_index(0)
+    data = np.asarray([sheet.row_values(i) for i in range(1, sheet.nrows)])
+    n_samples = sheet.nrows - 1
+    x_data = data[:,0].reshape(n_samples, 1)
+    y_data = data[:,1].reshape(n_samples, 1)
 
-    a = tf.constant([2, 3], name='a')
-    b = tf.constant([2, 5], name='b')
-    x = tf.add(a, b)
 
+    # Step 2: create placeholders for input X (number of fire) and label Y (number of theft)
+    X = tf.placeholder(tf.float32, shape=[None, 1], name = "X")
+    Y = tf.placeholder(tf.float32, shape=[None, 1], name = "Y")
+
+    # Step 3: create weight and bias, initialized to 0
+    w = tf.Variable(tf.zeros([1,1]), name='w')
+    b = tf.Variable(tf.zeros([1,1]), name='b')
+
+    # Step 4: predict Y (number of theft) from the number of fire
+    with tf.name_scope("Model"):
+        Y_predicted = tf.add(tf.matmul(X, w), b, name='Y_predicted')
+
+    # Step 5: use the square error as the loss function
+    with tf.name_scope("Loss"):
+        loss = tf.reduce_mean(tf.square(Y - Y_predicted, name = 'loss'))
+
+    # Step 6: using gradient descent with learning rate of 0.01 to minimize loss
+    with tf.name_scope("SDG"):
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
+
+
+    # Create operation to initialize all variables
+    init = tf.global_variables_initializer()
+
+    # Create a summary to monitor loss
+    training_summary = tf.summary.scalar("loss", loss)
+
+    # Phase 2: Train our model
     with tf.Session() as sess:
         writer = tf.summary.FileWriter('./graphs', sess.graph)
-        print(sess.run(x))
+        # Step 7: initialize the necessary variables, in this case, w and b
+        sess.run(init)
 
-    # close the writer when you’re done using it
-    writer.close()
+        # Step 8: train the model
+        cost_history = []
+        for i in range(20): # run 10 epochs
+            # Session runs optimizer to minimize loss and fetch the value of loss
+            _, loss_, summary, = sess.run([optimizer, loss, training_summary],
+                                          feed_dict={X: x_data, Y: y_data})
+            writer.add_summary(summary, i)
+
+            print("Epoch {0}: {1}".format(i, loss_))
+
+        # Step 9: output the values of w and b
+        w_value, b_value = sess.run([w, b])
+
+    # plot the results
+    X, Y = x_data, y_data
+    plt.plot(X, Y,  'bo',   label='Real data')
+    plt.plot(X, X * w_value + b_value,  'r',    label='Predicted data')
+    plt.legend()
+    plt.show()
 
 if __name__ == '__main__':
     main()
