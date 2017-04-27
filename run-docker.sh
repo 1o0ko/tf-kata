40b904d6171ed31de3a768a64f7d83053f8dcd4b
1o0ko <wojciech.stokowiec@gmail.com>
2017-04-27 19:01:56 +0000

    added docker script to run stuff in it

---
+++ run-docker.sh
@@ -0,0 +1,38 @@
+# set defaults
+USE_GPU=false
+HOST_PORT=9999
+GPU_IMAGE=tensorflow/tensorflow:latest-gpu
+CPU_IMAGE=tensorflow/tensorflow
+
+DATA=/mnt/data/
+MODELS=/mnt/models
+NAME=tf-containter
+
+# parse arguments
+while getopts gd:m:p: option
+do 
+    case "${option}" in 
+        g) USE_GPU='true';;
+        p) HOST_POST=${OPTARG};; 
+        d) DATA=${OPTARG};; 
+        m) MODELS=${OPTARG};; 
+        *) error "unexpected option ${flag}";;
+    esac
+done
+
+if $USE_GPU
+then
+    echo "Running docker on GPU"
+    nvidia-docker run -it -d \
+        --volume=$MODELS:/models \
+        --volume=$DATA:/data \
+	--name $NAME \
+	--volume=$(pwd):/workspace $GPU_IMAGE /bin/bash
+else
+    echo "Running docker on CPU"
+    docker run -it -d \
+	    --volume=$MODELS:/models \
+	    --volume=$DATA:/data \
+	    --name $NAME \
+	    --volume=$(pwd):/workspace $CPU_IMAGE /bin/bash
+fi
