62cab1527767ec6b12390ce33c53540a4bb9ef67
1o0ko <wojciech.stokowiec@gmail.com>
2017-10-24 22:01:06 +0000

    added stub for model saving

---
+++ saving/train.py
@@ -0,0 +1,59 @@
+"""
+Saving a simple model
+"""
+import tensorflow as tf
+
+from data import load_fire_theft
+
+
+def main():
+    # Phase 1: Assemble the graph
+
+    # Step 1: read in data from the .xls file
+    x_data, y_data = load_fire_theft()
+
+    # Step 2: create placeholders for input X (number of fire) and label Y (number of theft)
+    X = tf.placeholder(tf.float32, shape=[None, 1], name = "X")
+    Y = tf.placeholder(tf.float32, shape=[None, 1], name = "Y")
+
+    # Step 3: create weight and bias, initialized to 0
+    w = tf.Variable(tf.zeros([1,1]), name='w')
+    b = tf.Variable(tf.zeros([1,1]), name='b')
+
+    # Step 4: predict Y (number of theft) from the number of fire
+    with tf.name_scope("Model"):
+        Y_predicted = tf.add(tf.matmul(X, w), b, name='Y_predicted')
+
+    # Step 5: use the square error as the loss function
+    with tf.name_scope("Loss"):
+        loss = tf.reduce_mean(tf.square(Y - Y_predicted, name = 'loss'))
+
+    # Step 6: using gradient descent with learning rate of 0.01 to minimize loss
+    with tf.name_scope("SDG"):
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(loss)
+
+    # Create operation to initialize all variables
+    init = tf.global_variables_initializer()
+
+    # Create a saver
+    saver = tf.train.Saver()
+
+    # Phase 2: Train our model
+    with tf.Session() as sess:
+        # Step 7: initialize the necessary variables, in this case, w and b
+        sess.run(init)
+
+        # Step 8: train the model
+        for i in range(20): # run 10 epochs
+            # Session runs optimizer to minimize loss and fetch the value of loss
+            _, loss_, = sess.run([optimizer, loss],
+                                 feed_dict={X: x_data, Y: y_data})
+
+            print("Epoch {0}: {1}".format(i, loss_))
+
+        saver.save(sess, './checkpoints') 
+
+
+
+if __name__ == '__main__':
+    main()
