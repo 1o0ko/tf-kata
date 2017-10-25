4bf9df1a65ea0aa4a1508be62c9dd5e3b2683b67
1o0ko <wojciech.stokowiec@gmail.com>
2017-10-25 22:24:39 +0000

    added graph freezing and cleaning

---
+++ saving/freeze_graph.py
@@ -0,0 +1,65 @@
+"""
+Usage: train.py MODEL_DIR NODE_NAMES ...
+
+Arguments:
+    MODEL_DIR   path to tensorflow checkpoint
+    NODE_NAMES  list of nodes to export from graph
+
+Based on: https://blog.metaflow.fr/tensorflow-how-to-freeze-a-model-and-serve-it-with-a-python-api-d4f3596b3adc
+"""
+import os
+import logging
+import tensorflow as tf
+
+from typeopt import Arguments
+
+def freeze_graph(model_dir, node_names):
+    '''
+    Extracts the sub-graph defined by the nodes and converts
+    all its variables into constants
+    '''
+    logger = logging.getLogger(__name__)
+
+    if not tf.gfile.Exists(model_dir):
+        raise AssertionError(
+            "Export directory doesn't exists. Please specify an export directory: %s" % model_dir)
+
+    if not node_names:
+        logger.warning("You need to supply the name of a node")
+        return
+
+    checkpoint = tf.train.get_checkpoint_state(model_dir)
+    input_checkpoint = checkpoint.model_checkpoint_path
+
+    # We precise the file fullname of our freezed graph
+    output_graph = os.path.join(os.path.dirname(model_dir), "frozen_model.pb")
+    
+    # We clear devices to allow TensorFlow to control on which device it will load operations
+    clear_devices = True
+
+    # We start a session using a temporaty fresh Graph
+    with tf.Session(graph=tf.Graph()) as sess:
+        # We import the meta graph in the current default Graph
+        saver = tf.train.import_meta_graph(
+            "%s.meta" % input_checkpoint, clear_devices=True)
+
+        # Restore the weights
+        saver.restore(sess, input_checkpoint)
+
+        # use built-in TF helper to export variables to constants
+        output_graph_def = tf.graph_util.convert_variables_to_constants(
+            sess,                                   # session is used to retrieve the weights
+            tf.get_default_graph().as_graph_def(),  # The graph_def is used to retrieve the nodes 
+            node_names)                             # The nodes are used to select the subgraphs nodes
+
+        # Serialize and dump the output subgraph to the filesystem
+        with tf.gfile.GFile(output_graph, 'wb') as f:
+            f.write(output_graph_def.SerializeToString())
+
+         logger.info("%d ops in the source graph." % len(tf.get_default_graph().node))
+         logger.info("%d ops in the final graph." % len(output_graph_def.node))
+
+
+if __name__ == '__main__':
+    args = Arguments(__doc__, version='text')
+    freeze_graph(args.model_dir, args.node_names)
