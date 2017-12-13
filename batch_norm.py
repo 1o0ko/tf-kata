712c1f5067ab66aa402d71e34e20cd8f9efeb651
1o0ko <wojciech.stokowiec@gmail.com>
2017-12-13 17:41:48 -0500

    added sample data

--- batch_norm.py
+++ batch_norm.py
@@ -2,7 +2,13 @@
 Batch Normalization implementation in tensorflow:
     "Batch Normalization: Accelerating Deep Network Training by Reducing Internal Covariate Shift" (https://arxiv.org/abs/1502.03167)
 '''
+import numpy as np
 import tensorflow as tf
+import tqdm 
+
+from tensorflow.examples.tutorials.mnist import input_data
+
 
 if __name__ == '__main__':
+    mnist = input_data.read_data_sets("./datasets/mnist/", one_hot=True)
     print("yo!")
