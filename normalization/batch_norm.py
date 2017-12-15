c2097b37a5d2b72499d7e279b6ddd278b65c886d
1o0ko <wojciech.stokowiec@gmail.com>
2017-12-15 17:29:38 -0500

    moved files around

--- normalization/batch_norm.py
+++ normalization/batch_norm.py
@@ -2,13 +2,10 @@
 Batch Normalization implementation in tensorflow:
     "Batch Normalization: Accelerating Deep Network Training by Reducing Internal Covariate Shift" (https://arxiv.org/abs/1502.03167)
 '''
-import numpy as np
 import tensorflow as tf
-import tqdm 
 
-from tensorflow.examples.tutorials.mnist import input_data
-
+def batch_norm(inputs, is_training, decay = 0.999):
+    pass
 
 if __name__ == '__main__':
-    mnist = input_data.read_data_sets("./datasets/mnist/", one_hot=True)
     print("yo!")
