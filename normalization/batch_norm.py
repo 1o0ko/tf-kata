fd0329dd8919d815f6b7fd4c56301dd7198f6a86
1o0ko <wojciech.stokowiec@gmail.com>
2017-12-20 18:18:46 -0500

    added batch norm

--- normalization/batch_norm.py
+++ normalization/batch_norm.py
@@ -4,8 +4,32 @@ Batch Normalization implementation in tensorflow:
 '''
 import tensorflow as tf
 
-def batch_norm(inputs, is_training, decay = 0.999):
-    pass
+
+class batch_norm(object):
+  """Code modification of http://stackoverflow.com/a/33950177"""
+  def __init__(self, epsilon=1e-5, momentum = 0.1, name="batch_norm"):
+    with tf.variable_scope(name) as scope:
+      self.epsilon = epsilon
+      self.momentum = momentum
+
+      self.ema = tf.train.ExponentialMovingAverage(decay=self.momentum)
+      self.name=name
+
+  def __call__(self, x, train=True):
+    shape = x.get_shape().as_list()
+
+    with tf.variable_scope(self.name) as scope:
+      self.gamma = tf.get_variable("gamma", [shape[-1]],
+          initializer=tf.random_normal_initializer(1., 0.02))
+      self.beta = tf.get_variable("beta", [shape[-1]],
+          initializer=tf.constant_initializer(0.))
+
+      mean, variance = tf.nn.moments(x, [0, 1, 2])
+
+      return tf.nn.batch_norm_with_global_normalization(
+        x, mean, variance, self.beta, self.gamma, self.epsilon,
+        scale_after_normalization=True)
+
 
 if __name__ == '__main__':
     print("yo!")
